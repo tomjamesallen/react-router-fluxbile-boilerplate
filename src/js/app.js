421cf32bf32d353a00e81c5d281f1447ce0ae0b9
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-18 18:19:14 +0100

    Eslinting.

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,9 @@
  * Copyright 2014, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
-import Fluxible from 'fluxible';
-import ApplicationStore from './stores/ApplicationStore';
-import Routes from './components/Routes';
+import Fluxible from 'fluxible'
+import ApplicationStore from './stores/ApplicationStore'
+import Routes from './components/Routes'
 
 const app = new Fluxible({
   component: Routes,
--- src/js/client.js
+++ src/js/client.js
@@ -2,53 +2,46 @@
  * Copyright 2014, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
-/*global App, document, window */
-'use strict';
-var React = require('react');
-var ReactDOM = require('react-dom');
-var createBrowserHistory = require('history/lib/createBrowserHistory');
-var debug = require('debug');
-var bootstrapDebug = debug('Example');
-var app = require('./app');
-var dehydratedState = window.App; // Sent from the server
-var ReactRouter = require('react-router');
-var FluxibleComponent = require('fluxible-addons-react/FluxibleComponent');
-var createElement = require('fluxible-addons-react/createElementWithContext');
+/*global document, window */
+'use strict'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import debug from 'debug'
+import app from './app'
+const dehydratedState = window.App // Sent from the server
+import ReactRouter from 'react-router'
+import FluxibleComponent from 'fluxible-addons-react/FluxibleComponent'
 
-import routeActions from './actions/routeActions'
+var bootstrapDebug = debug('Example')
 
-bootstrapDebug('rehydrating app');
+bootstrapDebug('rehydrating app')
 
-function RenderApp(context){
-    bootstrapDebug('React Rendering');
+function RenderApp(context) {
+  bootstrapDebug('React Rendering')
 
-    const Router = React.createElement(ReactRouter.Router, {
-        routes: context.getComponent(),
-        history: require('./history'),
-        onUpdate: function() {
-            const { location, params } = this.state
-            context.executeAction(routeActions.CACHE_ROUTE_STATE, this.state)
-        }
-    })
-    
-    var mountNode = document.getElementById('app');
-    ReactDOM.render(
-        React.createElement(
-            FluxibleComponent,
-            { context: context.getComponentContext() },
-            Router
-        ),
-        mountNode,
-        function () {
-            bootstrapDebug('React Rendered');
-        }
-    );
+  const Router = React.createElement(ReactRouter.Router, {
+    routes: context.getComponent(),
+    history: require('./history')
+  })
+
+  var mountNode = document.getElementById('app')
+  ReactDOM.render(
+    React.createElement(
+      FluxibleComponent,
+      { context: context.getComponentContext() },
+      Router
+    ),
+    mountNode,
+    function() {
+      bootstrapDebug('React Rendered')
+    }
+  )
 }
 
-app.rehydrate(dehydratedState, function (err, context) {
-    if (err) throw err
-    window.debug = debug
-    window.context = context
+app.rehydrate(dehydratedState, function(err, context) {
+  if (err) throw err
+  window.debug = debug
+  window.context = context
 
-    RenderApp(context)
-});
+  RenderApp(context)
+})
