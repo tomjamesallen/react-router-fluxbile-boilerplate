ee9b90a3e112063ff8362958b3961c7a6f9d1657
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-20 14:28:58 +0100

    Including className prop in component template.

--- templates/Component.react.js
+++ templates/Component.react.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+// import React, { PropTypes } from 'react'
 // import { connectToStores, provideContext } from 'fluxible-addons-react'
 
 var {{COMPONENT}} = React.createClass({
@@ -9,7 +10,7 @@ var {{COMPONENT}} = React.createClass({
 
   render() {
     return (
-      <div>
+      <div className='{{COMPONENT_DASHED}}'>
         <h2>{{COMPONENT}}</h2>
       </div>
     )
